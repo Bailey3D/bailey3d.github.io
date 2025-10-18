a1d8cd184901e4ed6bcb4a47459b7f8cd1f977cb
Bailey Martin <bhmbob@gmail.com>
2025-10-18 11:17:28 +0100

    Add basic devcontainer setup + site config

---
+++ site/serve.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /workspace/site
+bundle exec jekyll serve --host 0.0.0.0 --port 4000 --livereload --livereload-port 35729 --incremental --config bailey3d.yml
+echo "Jekyll started with --livereload on port 35729"
